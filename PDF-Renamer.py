543c1961e48b58cbe0ec46c753cc6335365b8517
BujarMuharemi <bujar.muharemi.28@gmail.com>
2020-08-14 19:44:02 +0200

    runs now with pdfminer.six

--- PDF-Renamer.py
+++ PDF-Renamer.py
@@ -18,10 +18,10 @@ def getFiles(path):
         return [f for f in listdir(path) if isfile(join(path, f)) and f.endswith(".pdf")]
 
 
-pdfPath = r"C:\Users\Resul\Desktop\test3\\"
+pdfPath = r'D:/Documents/_DEVELOPMENT/Projects/Reifenhandel/PDF-Renamer/pdfs/'
 fileName = r"qwert"
 pdfName = fileName+r".pdf"
-pdt2textCMD = r"python C:\Users\Resul\AppData\Local\Programs\Python\Python37-32\Scripts\pdf2txt.py"
+pdt2textCMD = r"python C:\Users\Bujar\AppData\Local\Programs\Python\Python38-32\Scripts\pdf2txt.py"
 iban = ""
 
 #example cmd
@@ -33,26 +33,33 @@ files = getFiles(pdfPath)
 iban="_"
 
 for j in files:
-        #print(j)
+       
 
         #cmd = pdt2textCMD+" "+pdfPath+pdfName
-        cmd = pdt2textCMD+" "+pdfPath+j
+        jFix=r""+j
+        newName =""
+        #if(" " in pdfPath):
+                #newName = j.replace(r" ",r"_")
+
+#        os.rename(pdfPath+pdfName,pdfPath+newName)
+
+
+        cmd = pdt2textCMD+r' "'+pdfPath+jFix+r'"'
+        print(">>>>>>> "+cmd)
 
         #a=os.system(cmd)
 
-        stream = os.popen(cmd)
+        stream = os.popen(r""+cmd)
         output = stream.read()
 
         array=output.splitlines()
 
-
-
         for i in array:               
                 if(("Rechnung" or "RG") in i):
-                        #print(i)
+                        print(i)
                         iban = i
                         pdfName=j.split(".")[0]
-                        os.rename(pdfPath+pdfName+".pdf",pdfPath+pdfName+iban+".pdf")
+                        #os.rename(pdfPath+pdfName+".pdf",pdfPath+pdfName+iban+".pdf")
                 else:
                         iban = "-keineRechnung"
                         
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PdfRenamer
 Small script for renamings pdfs on an given query.
+
+[pdfminer lib](https://github.com/pdfminer/pdfminer.six)
