a2aa1308e38ecadc6b1ac845ba37eacc66a1a0c3
BujarMuharemi <bujar.muharemi.28@gmail.com>
2020-08-18 15:12:32 +0200

    added regex finding, renaming works

--- PDF-Renamer.py
+++ PDF-Renamer.py
@@ -12,6 +12,7 @@ TO_DO
 import os
 from os import listdir
 from os.path import isfile, join
+import re 
 
 
 def getFiles(path):
@@ -30,11 +31,11 @@ cmd = r"python C:\Users\Resul\AppData\Local\Programs\Python\Python37-32\Scripts\
 #print(getFiles(pdfPath))
 
 files = getFiles(pdfPath)
-iban="_"
 
-for j in files:
-       
 
+#looping threw all files
+for j in files:       
+        
         #cmd = pdt2textCMD+" "+pdfPath+pdfName
         jFix=r""+j
         newName =""
@@ -43,28 +44,31 @@ for j in files:
 
 #        os.rename(pdfPath+pdfName,pdfPath+newName)
 
-
         cmd = pdt2textCMD+r' "'+pdfPath+jFix+r'"'
-        print(">>>>>>> "+cmd)
-
-        #a=os.system(cmd)
+              
 
         stream = os.popen(r""+cmd)
         output = stream.read()
 
-        array=output.splitlines()
+        array=output.splitlines() # splitting the whole pdf by lines
 
-        for i in array:               
-                if(("Rechnung" or "RG") in i):
+        #looping threw each line
+        for i in array: 
+
+                #check if space is in line there                
+                lineCheck = re.search("\s[R][0-9]*$",i)
+                if(lineCheck):
+                        lineCheck = re.split("\s",i)
+                        print(lineCheck[1])
+                        os.rename(r""+pdfPath+j,pdfPath+lineCheck[1]+".pdf")
+
+
+                x = re.search("^R+[G]*[0-9]+",i)
+                if(x):                        
                         print(i)
-                        iban = i
-                        pdfName=j.split(".")[0]
-                        #os.rename(pdfPath+pdfName+".pdf",pdfPath+pdfName+iban+".pdf")
-                else:
-                        iban = "-keineRechnung"
+                        os.rename(r""+pdfPath+j,pdfPath+i+".pdf")                       
                         
-       
-        print(iban)
+               
         #os.rename(pdfPath+pdfName,pdfPath+fileName+iban+".pdf")
        
 
